0dee45d4b0a76d66148d17476dc20cf9fc289305
“VictoriaWW” <woltersvictoria@gmail.com>
2024-06-18 14:46:38 +0300

    created my test portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.from(".navbar-brand", {ease:"bounce", opacity:0, rotation:200, duration:2});
+gsap.from(".aboutMe", {ease:"elastic", opacity:0, duration:2, x:200, delay:1});
+gsap.from(".blockAM", {ease:"elastic", opacity:0, rotate:-200, duration:2, x:200, delay:2});
+
+
+
+
